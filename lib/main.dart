0195774d1abd3e2291072e20291c4c9106a9bedb
ToonnaEmeka <Toonnae63@gmail.com>
2024-09-15 20:42:56 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Image Toggle & Counter App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const CounterPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+  bool _showFirstImage = true;
+
+  
+  void _toggleImage() {
+    setState(() {
+      _showFirstImage = !_showFirstImage;
+    });
+  }
+
+  
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: const Text('Increment Counter'),
+            ),
+
+            
+            _showFirstImage
+                ? Image.asset('images/karate1.jpeg.jpg')
+                : Image.asset('images/karate2.jpeg.jpg'),
+
+            
+            ElevatedButton(
+              onPressed: _toggleImage,
+              child: const Text('Toggle Image'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
